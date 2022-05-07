563fd795d632c0f01039abb4e6605add3df61681
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2022-05-07 21:21:46 +0530

    Update botstatus_teletips.py

--- botstatus_teletips.py
+++ botstatus_teletips.py
@@ -54,8 +54,8 @@ async def main_teletips():
                 xxx_teletips += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Refreshes automatically</i>"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
                 print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(6300)
+                await asyncio.sleep(180)
                         
-app.run(main_teletips())
+app.run(main_teletips(180))
 
 #Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
